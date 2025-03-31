eccfe8bb9641198fbb03111e17035f42bdba0b5d
devEducaua <schokito4@gmail.com>
2025-03-31 04:24:55 -0300

    General Update without README

---
+++ polybar/polybar/launch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Fechar todas as instâncias do Polybar
+killall -q polybar
+
+# Esperar a barra ser encerrada completamente
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Condição para escolher qual barra carregar
+if [[ "$1" == "one" ]]; then
+    # Carregar apenas a terceira barra (bar3)
+    polybar -c ~/.config/polybar/one.ini &
+elif [[ "$1" == "min" ]]; then
+    polybar -c ~/.config/polybar/min.ini &
+elif [[ "$1" == "long" ]]; then
+    polybar -c ~/.config/polybar/long.ini &
+else
+    # Carregar bar1 e bar2 juntas
+    polybar -c ~/.config/polybar/config.ini barLeft &
+    polybar -c ~/.config/polybar/config.ini barRight &
+fi
+
